587c9eda22cb01b7117c89c93766b0cf11bba6fb
Ayhan Akbas <ayhanabi@users.noreply.github.com>
2022-05-19 19:12:35 +0300

    commit

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # mp3indirdur.py
 Python3 web scraping to download hit mp3 files
 
-I love to listen to music as I drive on the long ways. Listening to radios makes me bored and usually most of the hit radio stations are unavailable on the way.
-So I decided to write this downloader script to fetch hit songs from "mp3indirdur.mobi" website.  Script downloads top 100 list of turkish hit songs. You can copy and modify the script according to your needs.
+I enjoy listening to music while driving long distances. Listening to the radio bores me, and most of the popular radio stations are usually unavailable on the way. So I decided to write this downloader script to fetch hit songs from the "mp3indirdur.mobi" website. The Script downloads the top 100 list of Turkish hit songs. You can copy and modify the script according to your needs.
 
--- mp3indirdur.py
+++ mp3indirdur.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-
+#
+#. Downloader for Top 100 Turkish Hit Songs from mp3indirdur.mobi website.
+#
 # mp3indirdur.py  by ayhanabi
 # http://github/ayhanabi/mp3indirdur
 # 
 # pls. give credit if you modify/distribute/extend etc.
+#
+#  Last Update: May 19,2022
 
 
 import os
@@ -13,7 +17,7 @@ import re
 
 url = 'https://www.mp3indirdur.mobi/hit-mp3ler.asp'
 page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html5lib')
+soup = BeautifulSoup(page.content, 'html.parser')
 table = soup.find("div", attrs={"class": "ortaSolBlokIcerik"})
 table_data  = table.ul.find_all("li")
 print("Collecting Song Ids ...")
@@ -29,7 +33,7 @@ for index, table in enumerate(table_data):
 print("Downloading ...\n")
 for songId in songs:
         print(f"{songs[songId]}")
-        response = requests.get("https://cdn1.mp3indirdur.info/indir.asp?ID="+ songId +"&cdn=cdn1&linkKontrol=0", allow_redirects=True)
+        response = requests.get("https://cdn103.mp3indirdur.info/indir.asp?ID="+ songId +"&cdn=cdn103&linkKontrol=0", allow_redirects=True)
         downloaded_file = response.content
         if response.status_code == 200:
             with open(songs[songId], 'wb') as f:
